a6ec33ad434760098ca698418ae95996c3378e4b
gitaukariuki <gitaukariuki@gmail.com>
2024-11-08 11:36:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='gitaukariuki',
+    author_email='gitaukariuki@gmail.com',
+    description='HospitalManagementSystem'
+)
